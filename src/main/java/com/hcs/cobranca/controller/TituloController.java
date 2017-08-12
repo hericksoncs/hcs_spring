c0d9362dba43fedcf3456555a523662cea51df0f
herickson <herickson@hcs-Ubuntu>
2017-08-12 16:37:10 -0300

    HCS 12/08/2017 16:37

--- src/main/java/com/hcs/cobranca/controller/TituloController.java
+++ src/main/java/com/hcs/cobranca/controller/TituloController.java
@@ -2,12 +2,25 @@ package com.hcs.cobranca.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hcs.cobranca.model.Titulo;
 
 @Controller
+
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		
+		System.out.println(">>>>" + titulo.getDescricao());
+		
 		return "CadastroTitulo";
 	}
 	
