0b455469da4c08b3052c0d7309406a1fbab7fc7c
herickson_camilo <herickson_camilo@HCS.datarey.info>
2017-08-21 10:16:35 -0300

    HCS - 21/08/2017

--- src/main/java/com/hcs/cobranca/controller/TituloController.java
+++ src/main/java/com/hcs/cobranca/controller/TituloController.java
@@ -3,6 +3,7 @@ package com.hcs.cobranca.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hcs.cobranca.model.Titulo;
 
@@ -17,10 +18,9 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		
-		System.out.println(">>>>" + titulo.getDescricao());
-		
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		return "CadastroTitulo";
 	}
 	
