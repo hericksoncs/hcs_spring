e9a1559599886670012e6902b22d36dee1b0f9db
herickson <herickson@hcs-Ubuntu>
2017-08-21 13:42:29 -0300

    hteste

---
+++ src/main/java/com/hcs/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.hcs.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hcs.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
